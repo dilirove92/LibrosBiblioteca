31a6a531295f9c1614d9f87ada12356a23642273
dilirove92 <dilirove@yahoo.es>
2017-01-16 16:44:18 -0500

    Add files via upload

---
+++ CapaNegocio/BusquedaAvanzada.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Common;
+using Biblioteca.CapaDatos;
+
+namespace Biblioteca.CapaNegocio
+{
+    public class BusquedaAvanzada
+    {
+        /// <summary>
+        /// CAMPO IDENTIFICADOR DE LA TABLA
+        /// </summary>
+        public int Autor { get; set; }
+
+        /// <summary>
+        /// CAMPO DESCRIPCIÓN DE LA TABLA
+        /// </summary>
+        public string Titulo { get; set; }
+
+        /// <summary>
+        /// CAMPO MONTO DE LA TABLA
+        /// </summary>
+        public decimal Editor { get; set; }
+
+        /// <summary>
+        /// CAMPO DESCRIPCIÓN DE LA TABLA
+        /// </summary>
+        public string Materias { get; set; }
+
+        /// <summary>
+        /// CAMPO DESCRIPCIÓN DE LA TABLA
+        /// </summary>
+        public string Descriptores { get; set; }
+
+        /// <summary>
+        /// CAMPO DESCRIPCIÓN DE LA TABLA
+        /// </summary>
+        public string Cumplir { get; set; }
+
+        BaseDatos objetoBD = new BaseDatos();
+
+    }
+}
---
+++ CapaNegocio/BusquedaLibre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Common;
+using Biblioteca.CapaDatos;
+
+namespace Biblioteca.CapaNegocio
+{
+    public class BusquedaLibre
+    {
+        
+
+    }
+}
